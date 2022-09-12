4ac09ee842e0d9e51b6e58c79ad68709096c3366
Eben Vosloo <ebenvosloo2001@gmail.com>
2022-09-12 22:36:05 +0200

    added security

--- include/db.inc.php
+++ include/db.inc.php
@@ -1,8 +1,8 @@
 <?php
 
 $DB_SERVER_NAME = "localhost";
-$DB_USERNAME = "root";
-$DB_PASSWORD = "Eben@Vosloo2001";
+$DB_USERNAME = $_ENV['DB_USER'];
+$DB_PASSWORD = $_ENV['DB_USERPWD'];
 $DB_NAME = "loginsystem";
 
 $conn = mysqli_connect($DB_SERVER_NAME, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
